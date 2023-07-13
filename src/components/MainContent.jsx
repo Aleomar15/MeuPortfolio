5bd72ca23c2f24d6aafa41eb6b1f0213e0090977
Alexandre O Marinho <alexandreomar0202@gmail.com>
2023-07-13 16:29:22 -0300

    Social Networks

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,8 +1,8 @@
-
+import '../styles/components/maincontent.sass';
 
 const MainContent = () => {
   return (
-    <div>MainContent</div>
+    <main id='main-content'>MainContent</main>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import SocialNetworks from "./SocialNetworks";
+
 import Avatar from "../img/FtPerfil.png";
 
 import "../styles/components/sidebar.sass";
@@ -5,9 +7,8 @@ const Sidebar = () => {
   return (
     <aside id="sidebar">
         <img src={Avatar} alt="Alexandre Oliveira Marinho" />
-        <p>imagem</p>
         <p className="title">Desenvolvedor Back-End</p>
-        <p>redes sociais</p>
+       <SocialNetworks/>
         <p>informações de contato</p>
         <a href="" className="btn">Download Currículo</a>
     </aside>
