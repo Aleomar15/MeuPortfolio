6a1d6d46279d12e3bebe0fe8f32b2cd99e1c9e17
Alexandre O Marinho <alexandreomar0202@gmail.com>
2023-07-19 16:14:51 -0300

    Complemento

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -4,7 +4,7 @@ const AboutContainer = () => {
     <section className="about-container">
         <h2>Sobre</h2>
         <p>
-        Sou Alexandre, tenho 21 anos e busco a minha primeira oportunidade na área da programação. Em 2020, ingressei no curso de Bacharelado em Ciências da Computação e atualmente estou no 7º período. Durante esse tempo, venho desenvolvendo diversos projetos para colocar em prática todo o conhecimento adquirido, e sinto-me preparado para somar à equipe de programadores.  Além do meu comprometimento com os estudos, possuo facilidade de aprendizado, sou organizado, leal e possuo boa comunicação. Estou constantemente buscando me aprimorar e me manter atualizado com as últimas tendências e tecnologias do mercado.
+        Sou Alexandre, tenho 21 anos e busco a minha primeira oportunidade na área da programação. Em 2020, ingressei no curso de Bacharelado em Ciências da Computação e atualmente estou no 8º período. Durante esse tempo, venho desenvolvendo diversos projetos para colocar em prática todo o conhecimento adquirido, e sinto-me preparado para somar à equipe de programadores.  Além do meu comprometimento com os estudos, possuo facilidade de aprendizado, sou organizado, leal e possuo boa comunicação. Estou constantemente buscando me aprimorar e me manter atualizado com as últimas tendências e tecnologias do mercado.
         </p>
     </section>
   )
